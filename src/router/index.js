4c795df60d717f9c5c8f9150966d92463682cd1a
Mullen Andrew <andrew.mullen@avast.com>
2019-11-13 13:24:18 +0100

    added shop

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,37 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
-import About from '../views/About.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Shop from "../views/Shop.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: About
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    //component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+	{
+		path: "/",
+		name: "home",
+		component: Home
+	},
+	{
+		path: "/about",
+		name: "about",
+		component: About
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		//component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+	},
+	{
+		path: "/shop",
+		name: "shop",
+		component: Shop
+	}
 ];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+	mode: "history",
+	base: process.env.BASE_URL,
+	routes
 });
 
 export default router;
