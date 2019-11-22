33ebf80b08b38beb060e54de558087ec1ad6ba4a
Andrew Mullen <michalmullen@gmail.com>
2019-11-22 14:00:18 +0100

    added welcome + home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Welcome from "../views/Welcome.vue";
 import About from "../views/About.vue";
 import Menu from "../views/Menu.vue";
 import Basket from "../views/Basket.vue";
@@ -11,9 +11,9 @@ Vue.use(VueRouter);
 
 const routes = [
 	{
-		path: "/",
-		name: "home",
-		component: Home,
+		path: "/welcome",
+		name: "welcome",
+		component: Welcome,
 		meta: {
 			guest: true
 		}
@@ -65,6 +65,36 @@ const router = new VueRouter({
 	mode: "history",
 	base: process.env.BASE_URL,
 	routes
+});
+
+router.beforeEach((to, from, next) => {
+	if (to.matched.some(record => record.meta.requiresAuth)) {
+		if (localStorage.getItem("jwt") == null) {
+			next({
+				path: "/login",
+				params: { nextUrl: to.fullPath }
+			});
+		} else {
+			let user = JSON.parse(localStorage.getItem("user"));
+			if (to.matched.some(record => record.meta.is_admin)) {
+				if (user.is_admin == 1) {
+					next();
+				} else {
+					next({ name: "userboard" });
+				}
+			} else {
+				next();
+			}
+		}
+	} else if (to.matched.some(record => record.meta.guest)) {
+		if (localStorage.getItem("jwt") == null) {
+			next();
+		} else {
+			next({ name: "userboard" });
+		}
+	} else {
+		next();
+	}
 });
 
 export default router;
