c842c1a99e84f6d753999afb54c0f88d3b0f3f53
Mitchell Mullen <michalmullen@gmail.com>
2019-11-22 01:31:02 +0100

    login validation

--- src/main.js
+++ src/main.js
@@ -1,23 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import vuetify from "./plugins/vuetify";
-import axios from "axios";
-import VueAxios from "vue-axios";
-import Vuelidate from "vuelidate";
-import { ValidationProvider } from "vee-validate";
-
-// Register it globally
-// main.js or any entry file.
-Vue.component("ValidationProvider", ValidationProvider);
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import vuetify from './plugins/vuetify';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import * as VeeValidate from 'vee-validate';
 
 Vue.config.productionTip = false;
 
 new Vue({
-	router,
-	vuetify,
-	Vuelidate,
-	render: h => h(App)
-}).$mount("#app");
+  router,
+  vuetify,
+  VeeValidate,
+  render: h => h(App)
+}).$mount('#app');
 
-Vue.use(VueAxios, axios, Vuelidate);
+Vue.use(VueAxios, axios, VeeValidate);
