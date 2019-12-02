84dcad2efcdc0982c8f07c6dc90927296f71edf2
Andrew Mullen <michalmullen@gmail.com>
2019-12-02 15:20:41 +0100

    admin can now add and rm menu items

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -12,52 +12,51 @@
 				fluid
 				class="pa-4"
 			>
-				<v-data-table
-					:headers="headers"
-					:items="card.items"
-					class="elevation-1 p-4"
-				>
-					<template v-slot:top>
-						<v-toolbar flat color="white">
-							<v-toolbar-title>{{ card.day }}</v-toolbar-title>
-							<v-divider class="mx-4" inset vertical></v-divider>
-							<v-spacer></v-spacer>
-							<v-dialog v-model="dialog" max-width="500px">
-								<template v-slot:activator="{ on }">
-									<v-btn color="primary" dark class="mb-2" v-on="on"
-										>New Item</v-btn
-									>
-								</template>
-								<v-card>
-									<v-card-text>
-										<v-container>
-											<v-row v-for="item in menuItems" :key="item.title">
-												<v-checkbox
-													v-model="selected"
-													:label="item.title"
-													:value="item.title"
-												></v-checkbox>
-											</v-row>
-										</v-container>
-									</v-card-text>
+				<v-card>
+					<v-toolbar flat color="white">
+						<v-toolbar-title>{{ card.day }}</v-toolbar-title>
+						<v-divider class="mx-4" inset vertical></v-divider>
+						<v-spacer></v-spacer>
 
-									<v-card-actions>
-										<v-spacer></v-spacer>
-										<v-btn color="secondary" text @click="close">Cancel</v-btn>
-										<v-btn color="secondary" text @click="save(card.id)"
-											>Save</v-btn
+						<v-menu offset-y>
+							<template v-slot:activator="{ on }">
+								<v-btn color="primary" dark v-on="on">
+									Add Item
+								</v-btn>
+							</template>
+							<v-list>
+								<v-list-item
+									v-for="(item, index) in menuItems"
+									:key="index"
+									@click="save(card.id, index)"
+								>
+									<v-list-item-title>{{ item.title }}</v-list-item-title>
+								</v-list-item>
+							</v-list>
+						</v-menu>
+					</v-toolbar>
+					<v-simple-table>
+						<template v-slot:default>
+							<thead>
+								<tr>
+									<th class="text-left">Name</th>
+									<th class="text-left">Delete</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr v-for="item in card.items" :key="item.name">
+									<td>{{ item.title }}</td>
+
+									<td>
+										<v-icon @click="deleteItem(card.id, item.id)" class="pl-1"
+											>mdi-delete</v-icon
 										>
-									</v-card-actions>
-								</v-card>
-							</v-dialog>
-						</v-toolbar>
-					</template>
-					<template v-slot:card.action="{ item }">
-						<v-icon small @click="deleteItem(card.item)">
-							mdi-delete
-						</v-icon>
-					</template>
-				</v-data-table>
+									</td>
+								</tr>
+							</tbody>
+						</template>
+					</v-simple-table>
+				</v-card>
 			</v-flex>
 		</v-layout>
 	</v-container>
@@ -72,21 +71,7 @@ Vue.use(VueAxios, axios);
 
 export default {
 	data: () => ({
-		dialog: false,
-		headers: [
-			{
-				text: "Menu Item",
-				align: "left",
-				sortable: false,
-				value: "name"
-			},
-			{ text: "Actions", value: "action", sortable: false }
-		],
-		defaultItem: {
-			name: ""
-		},
 		menuItems: [],
-		selected: [],
 		cards: [
 			{
 				day: "Monday",
@@ -141,10 +126,9 @@ export default {
 	},
 
 	methods: {
-		deleteItem(item) {
-			const index = this.desserts.indexOf(item);
-			confirm("Are you sure you want to delete this item?") &&
-				this.desserts.splice(index, 1);
+		deleteItem(cardId, itemId) {
+			let index = this.cards[cardId].items.findIndex(obj => obj.id == itemId);
+			this.cards[cardId].items.splice(index, 1);
 		},
 
 		close() {
@@ -155,14 +139,8 @@ export default {
 			}, 300);
 		},
 
-		save(index) {
-			console.log(index);
-			console.log(this.selected);
-			// if (this.editedIndex > -1) {
-			// 	Object.assign(this.desserts[this.editedIndex], this.editedItem);
-			// } else {
-			// 	this.desserts.push(this.editedItem);
-			// }
+		save(cardId, itemId) {
+			this.cards[cardId].items.push(this.menuItems[itemId]);
 			this.close();
 		}
 	}
