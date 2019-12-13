1476789ac4e780ee77eae5e23e75e6f2a7e5a4ca
Andrew Mullen <michalmullen@gmail.com>
2019-12-13 13:26:30 +0100

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -58,14 +58,32 @@
 			>
 			<v-icon class="pl-1">mdi-leaf</v-icon>
 			<v-spacer></v-spacer>
-			<v-menu bottom left :close-on-content-click="false">
+			<template v-if="items.length === 0">
+				<v-btn icon>
+					<v-icon
+						@mouseover="show = true"
+						@mouseout="show = false"
+						@click="getSubtotal"
+						>mdi-cart</v-icon
+					>
+				</v-btn>
+			</template>
+			<v-menu
+				bottom
+				left
+				:close-on-content-click="false"
+				v-if="items.length != 0"
+			>
 				<template v-slot:activator="{ on }">
 					<v-btn icon v-on="on">
 						<v-badge v-model="show" color="#D7BC2F" class="text--color" left>
 							<template v-slot:badge>
 								<span>{{ items.length }}</span>
 							</template>
-							<v-icon @mouseover="show = true" @mouseout="show = false"
+							<v-icon
+								@mouseover="show = true"
+								@mouseout="show = false"
+								@click="getSubtotal"
 								>mdi-cart</v-icon
 							>
 						</v-badge>
@@ -80,24 +98,23 @@
 						<v-list-item-title>{{ item.title }}</v-list-item-title>
 						<v-text-field
 							v-model="item.order_amount"
-							class="mx-4"
-							label="Rows"
 							max="50"
 							min="1"
 							step="1"
 							style="width: 80px"
 							type="number"
+							@click="getSubtotal"
 						></v-text-field>
 						<v-list-item-action>
 							<v-btn icon>
-								<v-icon>mdi-minus</v-icon>
+								<v-icon @click="removeItem(item.id)">mdi-minus</v-icon>
 							</v-btn>
 						</v-list-item-action>
 					</v-list-item>
 					<v-divider></v-divider>
 					<v-list-item
 						><v-list-item-subtitle class="text-right"
-							>Subtotal: <b>30$ </b>
+							>Subtotal: <b>{{ subtotal }}$</b>
 						</v-list-item-subtitle>
 					</v-list-item>
 					<v-list-item
@@ -124,7 +141,8 @@ export default {
 		loggedIn: false,
 		account: "mdi-account",
 		admin: false,
-		items: []
+		items: [],
+		subtotal: 0
 	}),
 	created() {
 		// gets called when page is loaded
@@ -143,7 +161,7 @@ export default {
 		this.$root.$on("Menu", () => {
 			// updates basket for nav
 			// needs timeout to get updated data, if not will get old data
-			setTimeout(this.getBasket, 500);
+			setTimeout(this.getBasket, 400);
 		});
 	},
 	methods: {
@@ -169,8 +187,6 @@ export default {
 				Vue.axios
 					.get(`http://localhost/api/user/${localStorage.getItem("id")}`)
 					.then(response => {
-						console.log("changing data");
-						console.log(response.data);
 						localStorage.setItem("user", JSON.stringify(response.data));
 					});
 			}
@@ -179,6 +195,21 @@ export default {
 			if (localStorage.getItem("basket") != null) {
 				this.items = JSON.parse(localStorage.getItem("basket"));
 			}
+		},
+		getSubtotal() {
+			let total = 0;
+			this.items.forEach(element => {
+				total += element.price * element.order_amount;
+			});
+			this.subtotal = total;
+			this.getBasket;
+			localStorage.setItem("basket", JSON.stringify(this.items));
+		},
+		removeItem($id) {
+			this.items = this.items.filter(function(jsonObject) {
+				return parseInt(jsonObject.id) != $id;
+			});
+			this.getSubtotal;
 		}
 	}
 };
