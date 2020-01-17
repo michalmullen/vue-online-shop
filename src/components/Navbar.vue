d61317f66ed1c1bf7ccde8bbbcefab9212b858e8
Mitchell Mullen <michalmullen@gmail.com>
2020-01-17 07:05:45 +0100

    check endpoints work

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,235 +1,217 @@
 <template>
-	<div>
-		<v-navigation-drawer v-model="drawer" :width="200" app clipped temporary>
-			<v-list dense>
-				<v-list-item link to="/user">
-					<v-list-item-action>
-						<v-icon>{{ account }}</v-icon>
-					</v-list-item-action>
-					<v-list-item-content>
-						<v-list-item-title>{{ name }}</v-list-item-title>
-					</v-list-item-content>
-				</v-list-item>
-				<v-divider></v-divider>
-				<v-list-item link to="/">
-					<v-list-item-action>
-						<v-icon>mdi-home</v-icon>
-					</v-list-item-action>
-					<v-list-item-content>
-						<v-list-item-title>Home</v-list-item-title>
-					</v-list-item-content>
-				</v-list-item>
-				<v-list-item link to="/menu">
-					<v-list-item-action>
-						<v-icon>mdi-food-apple</v-icon>
-					</v-list-item-action>
-					<v-list-item-content>
-						<v-list-item-title>Menu</v-list-item-title>
-					</v-list-item-content>
-				</v-list-item>
-				<v-list-item link to="/admin" v-if="admin === true">
-					<v-list-item-action>
-						<v-icon>mdi-account-supervisor</v-icon>
-					</v-list-item-action>
-					<v-list-item-content>
-						<v-list-item-title>Admin</v-list-item-title>
-					</v-list-item-content>
-				</v-list-item>
-				<v-list-item link to="/about">
-					<v-list-item-action>
-						<v-icon>mdi-information</v-icon>
-					</v-list-item-action>
-					<v-list-item-content>
-						<v-list-item-title>About</v-list-item-title>
-					</v-list-item-content>
-				</v-list-item>
-			</v-list>
-			<template v-slot:append v-if="loggedIn === true">
-				<div class="pa-2">
-					<v-btn block color="secondary" @click="logout">Logout</v-btn>
-				</div>
-			</template>
-		</v-navigation-drawer>
-		<v-app-bar app clipped-left color="primary" dark>
-			<v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-			<v-spacer></v-spacer>
-			<v-toolbar-title class="font-weight-black" link to="/"
-				>Zero Waste Foods</v-toolbar-title
-			>
-			<v-icon class="pl-1">mdi-leaf</v-icon>
-			<v-spacer></v-spacer>
-			<template v-if="items.length === 0">
-				<v-btn icon>
-					<v-badge v-model="show" color="#D7BC2F" class="text--color" left>
-						<template v-slot:badge>
-							<span>{{ items.length }}</span>
-						</template>
-						<v-icon
-							@mouseover="show = true"
-							@mouseout="show = false"
-							@click="getSubtotal"
-							>mdi-cart</v-icon
-						>
-					</v-badge>
-				</v-btn>
-			</template>
-			<v-menu
-				v-model="menu"
-				bottom
-				left
-				:close-on-content-click="false"
-				v-if="items.length != 0"
-			>
-				<template v-slot:activator="{ on }">
-					<v-btn icon v-on="on">
-						<v-badge v-model="show" color="#D7BC2F" class="text--color" left>
-							<template v-slot:badge>
-								<span>{{ items.length }}</span>
-							</template>
-							<v-icon
-								@mouseover="show = true"
-								@mouseout="show = false"
-								@click="getSubtotal"
-								>mdi-cart</v-icon
-							>
-						</v-badge>
-					</v-btn>
-				</template>
+  <div>
+    <v-navigation-drawer v-model="drawer" :width="200" app clipped temporary>
+      <v-list dense>
+        <v-list-item link to="/user">
+          <v-list-item-action>
+            <v-icon>{{ account }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ name }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list-item link to="/">
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/menu">
+          <v-list-item-action>
+            <v-icon>mdi-food-apple</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Menu</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/admin" v-if="admin === true">
+          <v-list-item-action>
+            <v-icon>mdi-account-supervisor</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Admin</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/about">
+          <v-list-item-action>
+            <v-icon>mdi-information</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <template v-slot:append v-if="loggedIn === true">
+        <div class="pa-2">
+          <v-btn block color="secondary" @click="logout">Logout</v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
+    <v-app-bar app clipped-left color="primary" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-spacer></v-spacer>
+      <v-toolbar-title class="font-weight-black" link to="/">Zero Waste Foods</v-toolbar-title>
+      <v-icon class="pl-1">mdi-leaf</v-icon>
+      <v-spacer></v-spacer>
+      <template v-if="items.length === 0">
+        <v-btn icon>
+          <v-badge v-model="show" color="#D7BC2F" class="text--color" left>
+            <template v-slot:badge>
+              <span>{{ items.length }}</span>
+            </template>
+            <v-icon @mouseover="show = true" @mouseout="show = false" @click="getSubtotal">mdi-cart</v-icon>
+          </v-badge>
+        </v-btn>
+      </template>
+      <v-menu v-model="menu" bottom left :close-on-content-click="false" v-if="items.length != 0">
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-badge v-model="show" color="#D7BC2F" class="text--color" left>
+              <template v-slot:badge>
+                <span>{{ items.length }}</span>
+              </template>
+              <v-icon
+                @mouseover="show = true"
+                @mouseout="show = false"
+                @click="getSubtotal"
+              >mdi-cart</v-icon>
+            </v-badge>
+          </v-btn>
+        </template>
 
-				<v-list>
-					<v-list-item v-for="(item, i) in items" :key="i">
-						<v-list-item-avatar
-							><v-img :src="item.image"></v-img
-						></v-list-item-avatar>
-						<v-list-item-title>{{ item.title }}</v-list-item-title>
-						<v-text-field
-							v-model="item.order_amount"
-							max="50"
-							min="1"
-							step="1"
-							style="width: 80px"
-							type="number"
-							@click="getSubtotal"
-						></v-text-field>
-						<v-list-item-action>
-							<v-btn icon>
-								<v-icon @click="removeItem(item.id)">mdi-minus</v-icon>
-							</v-btn>
-						</v-list-item-action>
-					</v-list-item>
-					<v-divider></v-divider>
-					<v-list-item
-						><v-list-item-subtitle class="text-right"
-							>Subtotal: <b>{{ subtotal }}$</b>
-						</v-list-item-subtitle>
-					</v-list-item>
-					<v-list-item
-						><v-btn
-							block
-							color="primary"
-							link
-							to="/checkout"
-							@click="menu = false"
-							>Checkout</v-btn
-						></v-list-item
-					>
-				</v-list>
-			</v-menu>
-		</v-app-bar>
-	</div>
+        <v-list>
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-avatar>
+              <v-img :src="item.image"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-text-field
+              v-model="item.order_amount"
+              max="50"
+              min="1"
+              step="1"
+              style="width: 80px"
+              type="number"
+              @click="getSubtotal"
+            ></v-text-field>
+            <v-list-item-action>
+              <v-btn icon>
+                <v-icon @click="removeItem(item.id)">mdi-minus</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+          <v-divider></v-divider>
+          <v-list-item>
+            <v-list-item-subtitle class="text-right">
+              Subtotal:
+              <b>{{ subtotal }}$</b>
+            </v-list-item-subtitle>
+          </v-list-item>
+          <v-list-item>
+            <v-btn block color="primary" link to="/checkout" @click="menu = false">Checkout</v-btn>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
 import Vue from "vue";
 
 export default {
-	name: "Navbar",
-	props: {
-		source: String
-	},
-	data: () => ({
-		drawer: false,
-		show: false,
-		name: "No User",
-		loggedIn: false,
-		account: "mdi-account",
-		admin: false,
-		menu: false,
-		items: [],
-		subtotal: 0
-	}),
-	created() {
-		// gets called when page is loaded
-		this.checkIfIsLogged();
-		this.getBasket();
-	},
-	destroyed() {
-		localStorage.removeItem("menu");
-	},
-	watch: {
-		// call method if the route changes
-		$route: "checkIfIsLogged"
-	},
-	mounted() {
-		// listens for basket being updated in menu
-		this.$root.$on("Menu", () => {
-			// updates basket for nav
-			// needs timeout to get updated data, if not will get old data
-			setTimeout(this.getBasket, 400);
-		});
-	},
-	methods: {
-		closeMenu() {
-			this.menu = false;
-		},
-		logout() {
-			localStorage.clear();
-			Vue.axios.get("http://localhost/api/logout");
-			this.$router.push("Welcome");
-		},
-		checkIfIsLogged() {
-			if (localStorage.getItem("id") == null) {
-				this.loggedIn = false;
-				this.account = "mdi-account-off";
-				this.name = "No User";
-				this.admin = false;
-			} else {
-				let user = JSON.parse(localStorage.getItem("user"));
-				this.loggedIn = true;
-				this.account = "mdi-account";
-				this.name = user.name;
-				if (user.admin == 1) {
-					this.admin = true;
-				}
-				Vue.axios
-					.get(`http://localhost/api/user/${localStorage.getItem("id")}`)
-					.then(response => {
-						localStorage.setItem("user", JSON.stringify(response.data));
-					});
-			}
-		},
-		getBasket() {
-			if (localStorage.getItem("basket") != null) {
-				this.items = JSON.parse(localStorage.getItem("basket"));
-			}
-		},
-		getSubtotal() {
-			let total = 0;
-			this.items.forEach(element => {
-				total += element.price * element.order_amount;
-			});
-			this.subtotal = total;
-			localStorage.setItem("subtotal", total);
-			this.getBasket;
-			localStorage.setItem("basket", JSON.stringify(this.items));
-		},
-		removeItem($id) {
-			this.items = this.items.filter(function(jsonObject) {
-				return parseInt(jsonObject.id) != $id;
-			});
-			localStorage.setItem("basket", JSON.stringify(this.items));
-			this.getSubtotal;
-		}
-	}
+  name: "Navbar",
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: false,
+    show: false,
+    name: "No User",
+    loggedIn: false,
+    account: "mdi-account",
+    admin: false,
+    menu: false,
+    items: [],
+    subtotal: 0
+  }),
+  created() {
+    // gets called when page is loaded
+    this.checkIfIsLogged();
+    this.getBasket();
+  },
+  destroyed() {
+    localStorage.removeItem("menu");
+  },
+  watch: {
+    // call method if the route changes
+    $route: "checkIfIsLogged"
+  },
+  mounted() {
+    // listens for basket being updated in menu
+    this.$root.$on("Menu", () => {
+      // updates basket for nav
+      // needs timeout to get updated data, if not will get old data
+      setTimeout(this.getBasket, 400);
+    });
+  },
+  methods: {
+    closeMenu() {
+      this.menu = false;
+    },
+    logout() {
+      localStorage.clear();
+      Vue.axios.get("http://mitch.zerofood.cz/api/logout");
+      this.$router.push("Welcome");
+    },
+    checkIfIsLogged() {
+      if (localStorage.getItem("id") == null) {
+        this.loggedIn = false;
+        this.account = "mdi-account-off";
+        this.name = "No User";
+        this.admin = false;
+      } else {
+        let user = JSON.parse(localStorage.getItem("user"));
+        this.loggedIn = true;
+        this.account = "mdi-account";
+        this.name = user.name;
+        if (user.admin == 1) {
+          this.admin = true;
+        }
+        Vue.axios
+          .get(
+            `http://mitch.zerofood.cz/api/user/${localStorage.getItem("id")}`
+          )
+          .then(response => {
+            localStorage.setItem("user", JSON.stringify(response.data));
+          });
+      }
+    },
+    getBasket() {
+      if (localStorage.getItem("basket") != null) {
+        this.items = JSON.parse(localStorage.getItem("basket"));
+      }
+    },
+    getSubtotal() {
+      let total = 0;
+      this.items.forEach(element => {
+        total += element.price * element.order_amount;
+      });
+      this.subtotal = total;
+      localStorage.setItem("subtotal", total);
+      this.getBasket;
+      localStorage.setItem("basket", JSON.stringify(this.items));
+    },
+    removeItem($id) {
+      this.items = this.items.filter(function(jsonObject) {
+        return parseInt(jsonObject.id) != $id;
+      });
+      localStorage.setItem("basket", JSON.stringify(this.items));
+      this.getSubtotal;
+    }
+  }
 };
 </script>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,221 +1,213 @@
 <template>
-	<v-container class="fluid">
-		<v-row class="text-center">
-			<v-content>
-				<h1 class="display-3">
-					Order Food You Love With the Environment in Mind
-				</h1>
-				<p class="pt-4">
-					This website offers us the best quality food and beverages at a
-					reasonable price, and here is all natural and organic product which is
-					directly from the farm. They also maintain the food ecosystem. They
-					have the delivery option for different appetite. They also have the
-					chilled facility to maintain the maximum freshness of the food.
-				</p>
-			</v-content>
-			<v-content>
-				<v-container class="fill-height" fluid>
-					<v-row align="center" justify="center">
-						<v-col cols="12" sm="8" md="8">
-							<v-card class="elevation-12">
-								<v-toolbar color="primary" dark flat>
-									<v-tabs
-										v-model="tabs"
-										background-color="primary"
-										centered
-										grow
-										icons-and-text
-									>
-										<v-tab @click="reset">Login</v-tab>
-										<v-tab @click="reset">Register</v-tab>
-									</v-tabs>
-								</v-toolbar>
-								<v-tabs-items v-model="tabs">
-									<v-tab-item>
-										<v-card-text>
-											<v-form ref="form">
-												<v-text-field
-													label="E-mail"
-													name="email"
-													prepend-icon="mdi-email"
-													type="email"
-													v-model="loginEmail"
-													:rules="emailRules"
-													required
-												/>
+  <v-container class="fluid">
+    <v-row class="text-center">
+      <v-content>
+        <h1 class="display-3">Order Food You Love With the Environment in Mind</h1>
+        <p class="pt-4">
+          This website offers us the best quality food and beverages at a
+          reasonable price, and here is all natural and organic product which is
+          directly from the farm. They also maintain the food ecosystem. They
+          have the delivery option for different appetite. They also have the
+          chilled facility to maintain the maximum freshness of the food.
+        </p>
+      </v-content>
+      <v-content>
+        <v-container class="fill-height" fluid>
+          <v-row align="center" justify="center">
+            <v-col cols="12" sm="8" md="8">
+              <v-card class="elevation-12">
+                <v-toolbar color="primary" dark flat>
+                  <v-tabs v-model="tabs" background-color="primary" centered grow icons-and-text>
+                    <v-tab @click="reset">Login</v-tab>
+                    <v-tab @click="reset">Register</v-tab>
+                  </v-tabs>
+                </v-toolbar>
+                <v-tabs-items v-model="tabs">
+                  <v-tab-item>
+                    <v-card-text>
+                      <v-form ref="form">
+                        <v-text-field
+                          label="E-mail"
+                          name="email"
+                          prepend-icon="mdi-email"
+                          type="email"
+                          v-model="loginEmail"
+                          :rules="emailRules"
+                          required
+                        />
 
-												<v-text-field
-													id="password"
-													label="Password"
-													name="password"
-													prepend-icon="mdi-lock"
-													type="password"
-													v-model="loginPassword"
-													:rules="passRules"
-													required
-												/>
-											</v-form>
-										</v-card-text>
-										<v-card-actions>
-											<v-spacer />
-											<v-btn color="secondary" @click="login">Login</v-btn>
-										</v-card-actions>
-									</v-tab-item>
-									<v-tab-item>
-										<v-card-text>
-											<v-form ref="form">
-												<v-text-field
-													label="Name"
-													name="name"
-													v-model="name"
-													prepend-icon="mdi-account"
-													type="text"
-													:rules="nameRules"
-													required
-												/>
-												<v-text-field
-													label="Email"
-													name="email"
-													v-model="email"
-													prepend-icon="mdi-email"
-													type="email"
-													:rules="emailRules"
-													required
-												/>
-												<v-text-field
-													id="password"
-													label="Password"
-													v-model="password"
-													name="password"
-													prepend-icon="mdi-lock"
-													type="password"
-													:rules="passwordRules"
-													required
-												/>
-											</v-form>
-										</v-card-text>
-										<v-card-actions>
-											<v-spacer />
-											<v-btn color="secondary" @click="register"
-												>Register</v-btn
-											>
-										</v-card-actions>
-									</v-tab-item>
-									<v-overlay :absolute="absolute" :value="overlay">
-										<v-btn color="secondary" @click="overlay = false">{{
-											overlayData
-										}}</v-btn>
-									</v-overlay>
-								</v-tabs-items>
-							</v-card>
-						</v-col>
-					</v-row>
-				</v-container>
-			</v-content>
-		</v-row>
-	</v-container>
+                        <v-text-field
+                          id="password"
+                          label="Password"
+                          name="password"
+                          prepend-icon="mdi-lock"
+                          type="password"
+                          v-model="loginPassword"
+                          :rules="passRules"
+                          required
+                        />
+                      </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-spacer />
+                      <v-btn color="secondary" @click="login">Login</v-btn>
+                    </v-card-actions>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-card-text>
+                      <v-form ref="form">
+                        <v-text-field
+                          label="Name"
+                          name="name"
+                          v-model="name"
+                          prepend-icon="mdi-account"
+                          type="text"
+                          :rules="nameRules"
+                          required
+                        />
+                        <v-text-field
+                          label="Email"
+                          name="email"
+                          v-model="email"
+                          prepend-icon="mdi-email"
+                          type="email"
+                          :rules="emailRules"
+                          required
+                        />
+                        <v-text-field
+                          id="password"
+                          label="Password"
+                          v-model="password"
+                          name="password"
+                          prepend-icon="mdi-lock"
+                          type="password"
+                          :rules="passwordRules"
+                          required
+                        />
+                      </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-spacer />
+                      <v-btn color="secondary" @click="register">Register</v-btn>
+                    </v-card-actions>
+                  </v-tab-item>
+                  <v-overlay :absolute="absolute" :value="overlay">
+                    <v-btn color="secondary" @click="overlay = false">
+                      {{
+                      overlayData
+                      }}
+                    </v-btn>
+                  </v-overlay>
+                </v-tabs-items>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-content>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 import Vue from "vue";
 
 export default {
-	data() {
-		return {
-			loginEmail: "",
-			loginPassword: "",
-			name: "",
-			email: "",
-			password: "",
-			tabs: null,
-			absolute: true,
-			overlay: false,
-			overlayData: "",
-			emailRules: [
-				v => !!v || "E-mail is required",
-				v => /.+@.+\..+/.test(v) || "E-mail must be valid"
-			],
-			passRules: [
-				v => !!v || "Password is required",
-				v => 7 < v.length || "Name must be 8 or more characters long"
-			],
-			passwordRules: [
-				v => !!v || "Password is required",
-				v => 7 < v.length || "Name must be 8 or more characters long"
-			],
-			nameRules: [v => !!v || "Name is required"]
-		};
-	},
-	methods: {
-		login() {
-			if (this.$refs.form.validate()) {
-				Vue.axios
-					.post("http://localhost/api/login", {
-						email: this.loginEmail,
-						password: this.loginPassword
-					})
-					.then(response => {
-						let is_admin = response.data.admin;
-						localStorage.setItem("user", JSON.stringify(response.data));
-						localStorage.setItem("id", response.data.id);
-						if (localStorage.getItem("id") != null) {
-							this.setMenu;
-							this.$emit("loggedIn");
-							if (this.$route.params.nextUrl != null) {
-								this.$router.push(this.$route.params.nextUrl);
-							} else {
-								if (is_admin == 1) {
-									this.$router.push("admin");
-								} else {
-									this.$router.push("/");
-								}
-							}
-						}
-					})
-					.catch(error => {
-						console.log(error);
-						this.overlayData = error.response.data[0];
-						this.overlay = true;
-					});
-			}
-		},
-		register() {
-			if (this.$refs.form.validate()) {
-				Vue.axios
-					.post("http://localhost/api/user", {
-						name: this.name,
-						email: this.email,
-						password: this.password
-					})
-					.then(response => {
-						this.tabs = 0;
-						this.reset;
-						console.log(response);
-					})
-					.catch(error => {
-						console.log(error);
-						this.overlayData = error.response.data[0];
-						this.overlay = true;
-					});
-			}
-		},
-		reset() {
-			this.loginEmail = "";
-			this.loginPassword = "";
-			this.name = "";
-			this.email = "";
-			this.password = "";
-		},
-		setMenu() {
-			Vue.axios
-				.get("http://localhost/api/menu")
-				.then(response => {
-					localStorage.setItem("menu", response.data.menu);
-				})
-				.catch(error => {
-					console.log(error);
-				});
-		}
-	}
+  data() {
+    return {
+      loginEmail: "",
+      loginPassword: "",
+      name: "",
+      email: "",
+      password: "",
+      tabs: null,
+      absolute: true,
+      overlay: false,
+      overlayData: "",
+      emailRules: [
+        v => !!v || "E-mail is required",
+        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+      ],
+      passRules: [
+        v => !!v || "Password is required",
+        v => 7 < v.length || "Name must be 8 or more characters long"
+      ],
+      passwordRules: [
+        v => !!v || "Password is required",
+        v => 7 < v.length || "Name must be 8 or more characters long"
+      ],
+      nameRules: [v => !!v || "Name is required"]
+    };
+  },
+  methods: {
+    login() {
+      if (this.$refs.form.validate()) {
+        Vue.axios
+          .post("http://mitch.zerofood.cz/api/login", {
+            email: this.loginEmail,
+            password: this.loginPassword
+          })
+          .then(response => {
+            let is_admin = response.data.admin;
+            localStorage.setItem("user", JSON.stringify(response.data));
+            localStorage.setItem("id", response.data.id);
+            if (localStorage.getItem("id") != null) {
+              this.setMenu;
+              this.$emit("loggedIn");
+              if (this.$route.params.nextUrl != null) {
+                this.$router.push(this.$route.params.nextUrl);
+              } else {
+                if (is_admin == 1) {
+                  this.$router.push("admin");
+                } else {
+                  this.$router.push("/");
+                }
+              }
+            }
+          })
+          .catch(error => {
+            console.log(error);
+            this.overlayData = error.response.data[0];
+            this.overlay = true;
+          });
+      }
+    },
+    register() {
+      if (this.$refs.form.validate()) {
+        Vue.axios
+          .post("http://mitch.zerofood.cz/api/user", {
+            name: this.name,
+            email: this.email,
+            password: this.password
+          })
+          .then(response => {
+            this.tabs = 0;
+            this.reset;
+            console.log(response);
+          })
+          .catch(error => {
+            console.log(error);
+            this.overlayData = error.response.data[0];
+            this.overlay = true;
+          });
+      }
+    },
+    reset() {
+      this.loginEmail = "";
+      this.loginPassword = "";
+      this.name = "";
+      this.email = "";
+      this.password = "";
+    },
+    setMenu() {
+      Vue.axios
+        .get("http://mitch.zerofood.cz/api/menu")
+        .then(response => {
+          localStorage.setItem("menu", response.data.menu);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
 };
 </script>
